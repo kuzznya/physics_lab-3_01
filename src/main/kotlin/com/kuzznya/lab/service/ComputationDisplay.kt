5fef715dcda04371fb6be307f99ddc8237a7bf88
kuzznya <ikuz_2002@mail.ru>
2020-06-09 23:18:53 +0300

    feat: enable normal resize

--- src/main/kotlin/com/kuzznya/lab/service/ComputationDisplay.kt
+++ src/main/kotlin/com/kuzznya/lab/service/ComputationDisplay.kt
@@ -2,11 +2,13 @@ package com.kuzznya.lab.service
 
 import com.kuzznya.lab.model.Point
 import com.kuzznya.lab.model.TensionField
+import javafx.beans.Observable
 import javafx.beans.value.ChangeListener
 import javafx.scene.canvas.Canvas
 import javafx.scene.paint.Color
 import java.lang.RuntimeException
 import java.util.concurrent.CompletableFuture
+import kotlin.math.abs
 import kotlin.math.min
 import kotlin.math.pow
 
@@ -20,6 +22,9 @@ class ComputationDisplay (
 ) {
     private var data: ArrayList<ArrayList<Double>> = ArrayList()
 
+    private var lastDrawedWidth = 0
+    private var lastDrawedHeight = 0
+
     fun compute(): CompletableFuture<Unit> {
         val xMin = from.x
         val xMax = to.x
@@ -28,9 +33,9 @@ class ComputationDisplay (
 
         val ctx = canvas.graphicsContext2D
 
-        for (x in 0..canvas.width.toInt() step 3) {
+        for (y in 0..canvas.height.toInt() step 3) {
             data.add(ArrayList())
-            for (y in 0..canvas.height.toInt() step 3) {
+            for (x in 0..canvas.width.toInt() step 3) {
                 val tensionValue = field.getTension(
                     x / canvas.width * (xMax - xMin) + xMin,
                     y / canvas.height * (yMax - yMin) + yMin
@@ -43,6 +48,26 @@ class ComputationDisplay (
             }
         }
 
+        lastDrawedWidth = canvas.width.toInt()
+        lastDrawedHeight = canvas.height.toInt()
+
+        canvas.widthProperty().addListener { _, _, newValue ->
+            if (abs(newValue.toInt() - lastDrawedWidth) > 10 ) {
+                Router.primaryStage.isResizable = false
+                redraw().get()
+                Router.primaryStage.isResizable = true
+                lastDrawedWidth = newValue.toInt()
+            }
+        }
+        canvas.heightProperty().addListener { _ , _, newValue ->
+            if (abs(newValue.toInt() - lastDrawedHeight) > 10 ) {
+                Router.primaryStage.isResizable = false
+                redraw().get()
+                Router.primaryStage.isResizable = true
+                lastDrawedHeight = newValue.toInt()
+            }
+        }
+
         return CompletableFuture.completedFuture(Unit)
     }
 
@@ -52,15 +77,19 @@ class ComputationDisplay (
 
         val ctx = canvas.graphicsContext2D
 
-        for (y in 0..data.size) {
-            for (x in 0..data[y].size) {
+        ctx.fill = Color.hsb(240.0, 1.0, 1.0)
+        ctx.fillRect(0.0, 0.0, canvas.width, canvas.height)
+
+        for (y in data.indices) {
+            for (x in data[y].indices) {
                 val tensionValue = data[y][x]
                 val hue = min(((tensionValue - minTension) / (maxTension - minTension)).pow(1 / 1.8) * (360.0 - 240.0) + 240.0, 360.0)
                 ctx.fill = Color.hsb(hue, 1.0, 1.0)
-                ctx.fillRect(x / data[y].size.toDouble() * canvas.width,
-                    y / data.size.toDouble() * canvas.height,
-                    canvas.width / data[y].size.toDouble(),
-                    canvas.height / data.size.toDouble())
+                ctx.fillRect(x / data[y].size.toDouble() * canvas.width - canvas.width / data[y].size / 2.0,
+                    y / data.size.toDouble() * canvas.height - canvas.height / data.size / 2.0,
+                    canvas.width / data[y].size,
+                    canvas.height / data.size
+                )
             }
         }
 
